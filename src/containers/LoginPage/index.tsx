52da26423155985284d85634799edf0f2ba5636b
samyung0 <yungchinpang999@gmail.com>
2024-05-14 22:17:01 +0800

    updated px for login page

--- src/containers/LoginPage/index.tsx
+++ src/containers/LoginPage/index.tsx
@@ -1,14 +1,16 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import LoginForm from './LoginForm';
-import { Separator } from '~/components/Separator';
+import Image from "next/image";
+import Link from "next/link";
+import LoginForm from "./LoginForm";
+import { Separator } from "~/components/Separator";
 
 export const LoginPage = () => {
   return (
     <section className="flex h-[100vh] items-center justify-center bg-light-yellow dark:bg-primary-dark-gray dark:text-background-light-gray">
       <div className="max-h-[80dvh] w-[95vw]  overflow-auto rounded-lg border-2 border-black bg-white py-16 dark:bg-highlight-dark md:w-[50vw] md:min-w-[500px] md:max-w-[600px]">
-        <div className="mx-auto max-w-[500px] px-24">
-          <h1 className="pb-6 text-center font-mosk text-2xl font-bold md:text-3xl md:tracking-wider">Login</h1>
+        <div className="mx-auto max-w-[500px] px-12 sm:px-16 md:px-24">
+          <h1 className="pb-6 text-center font-mosk text-2xl font-bold md:text-3xl md:tracking-wider">
+            Login
+          </h1>
           <br />
 
           <LoginForm />
@@ -20,18 +22,26 @@ export const LoginPage = () => {
           </div>
 
           <div className="mx-auto flex w-[60%] items-center justify-evenly self-stretch md:w-full">
-            <a className="p-4 pt-0" aria-label="Login With Google" href="/login/google">
+            <a
+              className="p-4 pt-0"
+              aria-label="Login With Google"
+              href="/login/google"
+            >
               <Image
-                src={'/svg/logo-google.svg'}
+                src={"/svg/logo-google.svg"}
                 alt="Login With Google"
                 width={55}
                 height={55}
                 className="size-[45px] md:size-[55px]"
               />
             </a>
-            <a className="p-4 pt-0" aria-label="Login With Github" href="/login/github">
+            <a
+              className="p-4 pt-0"
+              aria-label="Login With Github"
+              href="/login/github"
+            >
               <Image
-                src={'/svg/logo-github.svg'}
+                src={"/svg/logo-github.svg"}
                 alt="Login With Github"
                 width={50}
                 height={50}
@@ -41,7 +51,11 @@ export const LoginPage = () => {
           </div>
 
           <div className="text-center md:pt-4">
-            <Link prefetch href="/signup" className="inline-block text-sm underline underline-offset-[6px]">
+            <Link
+              prefetch
+              href="/signup"
+              className="inline-block text-sm underline underline-offset-[6px]"
+            >
               New User? Click Here to Sign up
             </Link>
           </div>
